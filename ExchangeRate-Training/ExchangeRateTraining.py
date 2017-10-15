968ed452776ece78831e33b43f99002a47b7ebed
GayathreeIyer <gimc6@mail.umkc.edu>
2017-10-15 14:04:09 -0500

    exchange_rate checkin

--- ExchangeRate-Training/ExchangeRateTraining.py
+++ ExchangeRate-Training/ExchangeRateTraining.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas
 from statsmodels.formula.api import ols
 from statsmodels.stats.anova import anova_lm
+import patsy
 import csv
 
 with open('Datasets/Exchangerate.csv') as csvfile:
@@ -21,4 +22,13 @@ with open('Datasets/Exchangerate.csv') as csvfile:
         months.append(month)
         years.append(year)
         rates.append(rate)
-print(weeks)
+
+weeks_flatten = np.ndarray.flatten(np.array(weeks))
+months_flatten = np.ndarray.flatten(np.array(months))
+years_flatten = np.ndarray.flatten(np.array(years))
+rates_flatten = np.ndarray.flatten(np.array(rates))
+x = np.column_stack((weeks_flatten,months_flatten,years_flatten))
+print(weeks_flatten)
+data = pandas.DataFrame({'Week':weeks_flatten, 'Month':months_flatten, 'Year':years_flatten, 'Rate':rates_flatten})
+print(data)
+
