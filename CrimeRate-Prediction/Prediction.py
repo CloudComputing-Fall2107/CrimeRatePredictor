5bbdd5dd56c1883749928961a1e04c6bae80f451
bharathkumarna <bharathkumar10795@gmail.com>
2017-11-06 22:58:13 -0600

    mofify dataframe to csv

--- CrimeRate-Prediction/Prediction.py
+++ CrimeRate-Prediction/Prediction.py
@@ -12,9 +12,20 @@ loaded_model3 = load_pickle('model3.pickle')
 
 #predict-data
 result1 = loaded_model1.predict(df1).astype('int')
-result1.to_csv('result1.csv')
+df_new_1 = pd.DataFrame(result1).join(df1)
+new_cols_1 = ['X1','X2','X3','X4','X5','X6','X7','X8']
+df_new_1.columns = new_cols_1
+df_new_1.to_csv('result1.csv')
+
 result2 = loaded_model2.predict(df2).astype('int')
-result2.to_csv('result2.csv')
+df_new_2 = pd.DataFrame(result2).join(df2)
+new_cols_2 = ['X3','X1','X2','X8']
+df_new_2.columns = new_cols_2
+df_new_2.to_csv('result2.csv')
+
 result3 = loaded_model3.predict(df3).astype('int')
-result3.to_csv('result3.csv')
+df_new_3 = pd.DataFrame(result3).join(df3)
+new_cols_3 = ['X4','X5','X6','X7','X1','X2','X3','X8']
+df_new_3.columns = new_cols_3
+df_new_3.to_csv('result3.csv')
 
